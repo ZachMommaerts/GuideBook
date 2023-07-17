614f14fbb0ff012de0081fb4c15b5d47ad18354d
ZachMommaerts <zmommaerts@gmail.com>
2023-07-17 15:26:06 -0500

    Initial Commit

---
+++ GuideBook/GuideBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideBookApp.swift
+//  GuideBook
+//
+//  Created by Zach Mommaerts on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct GuideBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
